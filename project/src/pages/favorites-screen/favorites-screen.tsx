c36171d8c9b084ab36dc39654713e55eb400b9b4
Olga Latkina <olga.latkina@gmail.com>
2022-07-22 22:21:08 +0700

    feat: add FavoriteEmpty component

--- project/src/pages/favorites-screen/favorites-screen.tsx
+++ project/src/pages/favorites-screen/favorites-screen.tsx
@@ -1,5 +1,6 @@
+import {useState, useEffect} from 'react';
 import {OffersType} from '../../types/offers';
-import FavoritesScreenEmpty from '../favorites-screen-empty/favorites-screen-empty';
+import FavoritesEmpty from '../../components/favorites-empty/favorites-empty';
 import Footer from '../../components/footer/footer';
 import FavoritesItem from '../../components/favorites-item/favorites-item';
 
@@ -23,22 +24,29 @@ const indexOffersByCities = (offers: OffersType): OffersIndexType => (
 );
 
 const FavoritesScreen = ({offers}: FavoritesScreenProps): JSX.Element => {
+  const [isEmpty, setIsEmpty] = useState(true);
   const favoritesOffers = offers.filter((offer) => offer.isFavorite);
-  if (favoritesOffers.length === 0) {
-    return <FavoritesScreenEmpty/>;
-  }
+
+  useEffect(() => {
+    if (favoritesOffers.length !== 0) {
+      setIsEmpty(false);
+    }
+  }, [isEmpty]);
 
   const indexedOffers = indexOffersByCities(offers);
 
   return (
     <>
-      <main className="page__main page__main--favorites">
+      <main className={`page__main page__main--favorites ${isEmpty ? 'page__main--favorites-empty' : ''}`}>
         <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {Object.entries(indexedOffers).map(([city, localOffers]) => <FavoritesItem cityName={city} localOffers={localOffers} key={city}/>)}
-            </ul>
+          <section className={`favorites ${isEmpty ? 'favorites--empty' : ''}`}>
+            <h1 className={isEmpty ? 'visually-hidden' : 'favorites__title'}>Saved listing</h1>
+            {isEmpty
+              ? <FavoritesEmpty />
+              :
+              <ul className="favorites__list">
+                {Object.entries(indexedOffers).map(([city, localOffers]) => <FavoritesItem cityName={city} localOffers={localOffers} key={city}/>)}
+              </ul>}
           </section>
         </div>
       </main>
