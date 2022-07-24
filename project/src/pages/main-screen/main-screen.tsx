a2773e839d6dc9858335706ebab2ff04bb5a5203
Olga Latkina <olga.latkina@gmail.com>
2022-07-24 12:11:50 +0700

    feat: add MainEmpty component

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -2,41 +2,48 @@ import {useEffect, useState} from 'react';
 import {cities} from '../../constants';
 import {OffersType} from '../../types/offers';
 import {UserType} from '../../types/user';
-
+import cn from 'classnames';
 import Header from '../../components/header/header';
 import TabItem from '../../components/tab-item/tab-item';
 import OffersList from '../../components/offers-list/offers-list';
 import FormSorting from '../../components/form-sorting/form-sorting';
+import MainEmpty from '../../components/main-no-offers/main-empty';
 
 type MainScreenProps = {
   offers: OffersType,
   user: UserType,
 }
 
-const filterOffersByCity = (offers: OffersType, city: string): OffersType => offers.filter((offer) => offer.city.name === city);
+const filterOffersByCity = (offers: OffersType, city: string): OffersType => (
+  offers.filter((offer) => offer.city.name === city)
+);
 
 const MainScreen = ({offers, user}: MainScreenProps): JSX.Element => {
   const [activeTab, setActiveTab] = useState('');
-  const [numberOfOffers, setNumberOfOffers] = useState(0);
+  const [filteredOffers, setFilteredOffers] = useState<OffersType>([]);
 
   useEffect(() => {
     setActiveTab('Amsterdam');
-    setNumberOfOffers(filterOffersByCity(offers, 'Amsterdam').length);
+    setFilteredOffers(filterOffersByCity(offers, 'Amsterdam'));
   }, []);
 
   const handleTabClick = (city: string) => {
     setActiveTab(city);
-    setNumberOfOffers(filterOffersByCity(offers, city).length);
+    setFilteredOffers(filterOffersByCity(offers, city));
   };
 
-  const getTitle = (numberOfCities: number) => (
-    `${numberOfCities} ${numberOfCities === 1 ? 'place' : 'places'} to stay in ${activeTab}`
+  const getTitle = (numberOfOffers: number) => (
+    `${numberOfOffers} ${numberOfOffers === 1 ? 'place' : 'places'} to stay in ${activeTab}`
   );
+
+  const mainClassName = cn('page__main page__main--index', {
+    'page__main--index-empty': filteredOffers.length === 0,
+  });
 
   return (
     <>
       <Header user={user}/>
-      <main className="page__main page__main--index">
+      <main className={mainClassName}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -51,18 +58,23 @@ const MainScreen = ({offers, user}: MainScreenProps): JSX.Element => {
             </ul>
           </section>
         </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{getTitle(numberOfOffers)}</b>
-              <FormSorting/>
-              <OffersList offers={offers}/>
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map"/>
+        <div className="cities">{
+          filteredOffers.length === 0
+            ?
+            <MainEmpty city={activeTab}/>
+            :
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{getTitle(filteredOffers.length)}</b>
+                <FormSorting/>
+                <OffersList offers={filteredOffers}/>
+              </section>
+              <div className="cities__right-section">
+                <section className="cities__map map"/>
+              </div>
             </div>
-          </div>
+        }
         </div>
       </main>
     </>
