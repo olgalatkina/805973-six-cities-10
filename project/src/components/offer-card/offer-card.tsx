25899fcaf75436cb903a3095f6c02b784bf89c66
Olga Latkina <olga.latkina@gmail.com>
2022-08-09 12:50:28 +0700

    feat: add Math.round for raiting

--- project/src/components/offer-card/offer-card.tsx
+++ project/src/components/offer-card/offer-card.tsx
@@ -62,7 +62,7 @@ const OfferCard = ({offer, onOfferMouseOver, onOfferMouseLeave}: OfferCardProps)
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: `${rating * 20}%`}}/>
+            <span style={{width: `${Math.round(rating) * 20}%`}}/>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
