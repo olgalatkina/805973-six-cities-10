4501208a63f0d65e9f0c77d0b1c199509c2a8e2a
Olga Latkina <olga.latkina@gmail.com>
2022-08-11 14:31:45 +0700

    refactor: styles in loading and error screens

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,8 +1,9 @@
 import {Navigate} from 'react-router-dom';
-import {AppRoute, AuthorizationStatus} from '../../constants';
+import {AppRoute, AuthorizationStatus, Status} from '../../constants';
 import Loading from '../loading/loading';
 import {useAppSelector} from '../../hooks';
-import {getAuthStatus} from '../../store/user-process/selectors';
+import {getAuthStatus, getStatusCheckAuth} from '../../store/user-process/selectors';
+import SomethingWrong from '../something-wrong/something-wrong';
 
 type PrivateRouteProps = {
   children: JSX.Element;
@@ -12,10 +13,17 @@ const isCheckedAuth = (authorizationStatus: string): boolean => authorizationSta
 
 const PrivateRoute = ({children}: PrivateRouteProps): JSX.Element => {
   const authorizationStatus = useAppSelector(getAuthStatus);
+  const status = useAppSelector(getStatusCheckAuth);
 
-  if (isCheckedAuth(authorizationStatus)) {
+  if (status === Status.Loading || status === Status.Idle || isCheckedAuth(authorizationStatus)) {
     return (
       <Loading />
+    );
+  }
+
+  if (status === Status.Error) {
+    return (
+      <SomethingWrong />
     );
   }
 
--- project/src/components/something-wrong/something-wrong.tsx
+++ project/src/components/something-wrong/something-wrong.tsx
@@ -1,8 +1,21 @@
+import cn from 'classnames';
+import styles from './something-wrong.module.css';
+
 const SomethingWrong = (): JSX.Element => (
-  <>
-    <h2>Something went wrong</h2>
-    <button onClick={() => window.location.reload()}>Click to reload!</button>
-  </>
+  <main className="page__main page__main--favorites page__main--favorites-empty">
+    <div className="page__favorites-container container">
+      <section className="favorites favorites--empty">
+        <h1 className="visually-hidden">Something went wrong</h1>
+        <div className={cn('wrapper', styles.wrapper)}>
+          <b className="favorites__status">Something went wrong.</b>
+          <p className="favorites__status-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+          </p>
+          <button className={styles.button} onClick={() => window.location.reload()}>Click to reload!</button>
+        </div>
+      </section>
+    </div>
+  </main>
 );
 
 export default SomethingWrong;
