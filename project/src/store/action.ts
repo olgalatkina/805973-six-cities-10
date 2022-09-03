dcf167cb0966b83fae71b751ca673acd93120ee6
Olga Latkina <olga.latkina@gmail.com>
2022-09-04 01:00:25 +0700

    refactor: remove unnecessary

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,21 +1,4 @@
 import {createAction} from '@reduxjs/toolkit';
 import {AppRoute} from '../constants';
 
-// export const changeActiveCity = createAction<string>('changeActiveCity');
-// export const setActiveSortType = createAction<string>('setActiveSortType');
-
-// export const loadOffers = createAction<OffersType>('loadOffers');
-// export const setDataLoadedStatus = createAction<boolean>('setDataLoadedStatus');
-
-// export const loadActiveOffer = createAction<OfferType>('loadOffer');
-// export const setOfferLoadedStatus = createAction<boolean>('setOfferLoadedStatus');
-// export const loadReviews = createAction<ReviewsType>('loadReviews');
-// export const loadNeighbourhood = createAction<OffersType>('loadNeighbourhood');
-
-// export const requireAuthorization = createAction<AuthorizationStatus>('requireAuthorization');
-// export const setUser = createAction<UserType | null>('setUser');
-
-// export const loadFavorites = createAction<OffersType>('loadFavorites');
-// export const changeFavoriteStatus = createAction<OfferType>('changeFavoriteStatus');
-
 export const redirectToRoute = createAction<AppRoute>('redirectToRoute');
--- project/src/store/reviews-data/selectors.ts
+++ project/src/store/reviews-data/selectors.ts
@@ -3,5 +3,4 @@ import {StateType} from '../../types/state';
 import {ReviewsType} from '../../types/reviews';
 
 export const getReviews = (state: StateType): ReviewsType => state[NameSpace.Reviews].reviews;
-export const getStatusAll = (state: StateType): string => state[NameSpace.Reviews].statusAll;
 export const getStatusPost = (state: StateType): string => state[NameSpace.Reviews].statusPost;
